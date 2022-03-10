c6f4737402667b0053fc8e2100a45a66e23a7a68
Edgar Klepek <edgarklepek@gmail.com>
2022-03-10 12:18:19 +0100

    first commit

---
+++ Projekte/voter/app.js
@@ -0,0 +1,43 @@
+const app = Vue.createApp({
+  // Optionen
+  data: function () {
+    return {
+      submissions: submissions, // Daten aus seed.js
+    };
+  },
+  methods: {
+    // upvote: function () {} // Funktionales Equivalent
+
+    // Normale Funktionsschreibweise ohne Klammern beim Aufruf
+    /*
+    upvote() {
+      this.submissions[0].votes++;
+
+      console.log(this);  // this-Objekt zeigt auf die Root-Component
+      console.log(event); // event-Objekt wird automatisch von Vue übergeben bei Funktionsaufruf ohne Klammern. Mit Klammern werden nur explizit angegebene Parameter übergeben.
+    },
+    */
+
+    // Normale Funktionsschreibeweise mit Klammern und eigenen Parametern/Argumenten
+    upvote(infoText, event) {
+      this.submissions[0].votes++;
+
+      console.log(this);  // this-Objekt zeigt auf die Root-Component
+      console.log(infoText); // expliziter Parameter
+      console.log(event);    // Event-Parameter explizit angegeben
+    },
+    logConsole(text) {
+      console.log(text);
+    },
+
+    // Keine Arrow-Functions nutzen, da this hier nicht nutzbar ist (zeigt auf Window-Objekt)!
+    /*
+    upvote() {
+      console.log(this);
+    },
+    */
+  }
+});
+
+// Liefert die Instanz zur Root-Component zurück
+const vm = app.mount("#app");
